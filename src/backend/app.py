8027a454804e666af7141f6cc8b956e8b151917a
GusSampaio <gustavo.sampaio@usp.br>
2025-10-05 15:06:03 -0300

    Using embedding model from HF API

--- src/backend/app.py
+++ src/backend/app.py
@@ -2,68 +2,78 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from pymongo import MongoClient
 from langchain_mistralai import ChatMistralAI
-# from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain_huggingface.embeddings.huggingface import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEndpointEmbeddings
 from langchain_mongodb import MongoDBAtlasVectorSearch
 from dotenv import load_dotenv
 from fastapi.middleware.cors import CORSMiddleware
 import spacy
 import os
 import re
+
+# --- Configurações ---
+# Carrega variáveis de ambiente de forma segura
 dotenv_path = "../../.env"
 load_dotenv(dotenv_path=dotenv_path)
 
+# Chaves de API e Conexões
+MONGODB_URI = os.environ["MONGODB_URI"]
+MISTRAL_API_KEY = os.environ["MISTRAL_API_KEY"]
+HUGGINGFACE_API_KEY = os.environ["HF_TOKEN"]
+
+# Configurações do MongoDB
+MONGODB_DATABASE = "minha_database"
+MONGODB_COLLECTION = "minha_collection"
+VECTOR_INDEX_PT = 'vector_index_pt'
+VECTOR_INDEX_EN = 'vector_index_en'
+
+# Modelos
+EMBEDDING_MODEL = "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
+CHAT_MODEL = "mistral-small"
+
+# --- Inicialização dos Clientes e Modelos ---
 app = FastAPI()
 
 # Configuração CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:9002"],  
+    allow_origins=["http://localhost:3000", "http://localhost:9002"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-MONGODB_DATABASE = "minha_database"
-MONGODB_COLLECTION = "minha_collection"
+# Cliente MongoDB
+client = MongoClient(MONGODB_URI)
+collection = client[MONGODB_DATABASE][MONGODB_COLLECTION]
 
-client = MongoClient(os.environ.get("MONGODB_URI"))
-db = client[MONGODB_DATABASE]
-collection = db[MONGODB_COLLECTION]
-
-embedding_generator = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
+## Instancia o gerador de embeddings usando a API de Inferência
+embedding_generator = HuggingFaceEndpointEmbeddings(
+    huggingfacehub_api_token=HUGGINGFACE_API_KEY,
+    model=EMBEDDING_MODEL,
+    task="feature-extraction",
 )
 
+# Vector Stores
 vector_store_pt = MongoDBAtlasVectorSearch(
-    embedding=embedding_generator,
     collection=collection,
-    index_name='vector_index_pt',
-    relevance_score_fn="cosine",
-    type="vectorSearch",
+    embedding=embedding_generator,
+    index_name=VECTOR_INDEX_PT,
 )
 
 vector_store_en = MongoDBAtlasVectorSearch(
-    embedding=embedding_generator,
     collection=collection,
-    index_name='vector_index_en',
-    relevance_score_fn="cosine",
-    type="vectorSearch",
+    embedding=embedding_generator,
+    index_name=VECTOR_INDEX_EN,
 )
 
-# Configuração do chat
-chat = ChatMistralAI(
-    model="mistral-small",
-    api_key=os.environ["MISTRAL_API_KEY"],
-)
+# Modelo de Chat
+chat = ChatMistralAI(model=CHAT_MODEL, api_key=MISTRAL_API_KEY)
 
-class Query(BaseModel):
-    query: str
-    lang: str
-
+# Carregamento dos modelos Spacy
 nlp_en = spacy.load("en_core_web_sm")
 nlp_pt = spacy.load("pt_core_news_sm")
 
+# --- Funções Auxiliares ---
 def clean_title(title: str) -> str:
     """Remove numeração do início dos títulos."""
     return re.sub(r'^\d+\.\s*', '', title).strip()
@@ -78,72 +88,56 @@ def clean_query(query: str, lang: str = "en") -> str:
     ]
     return " ".join(tokens)
 
+class Query(BaseModel):
+    query: str
+    lang: str
+
 @app.post("/search")
 async def search(q: Query):
-
     cleaned_query = clean_query(q.query, lang=q.lang)
-    print("Cleaned query:", cleaned_query)
+    if cleaned_query == "":
+        return {"answer": ""}
 
-    results=[]
+    results = []
     if q.lang == "pt":
         results = vector_store_pt.similarity_search_with_relevance_scores(q.query, k=5)
     else:
         results = vector_store_en.similarity_search_with_relevance_scores(q.query, k=5)
-    print(results[0])
-    
-    threshold = 0.65  # (similaridade de cosseno ~ entre -1 e 1)
-    filtered_results = [doc for doc, score in results if (score >= threshold) and (doc.metadata.get("lang") == q.lang)]
-    print(len(filtered_results))
-    if not filtered_results:
-        if q.lang=="pt":
-            return {"answer": "A resposta à sua pergunta não está diretamente relacionada ao conteúdo disponível no blog de Gustavo Sampaio. O blog trata principalmente de tópicos técnicos em tecnologia, ciência de dados, machine learning e inteligência artificial. Se tiver sugestões de novos assuntos, fique à vontade para entrar em contato pela página de contatos.",
-                    "sources": []}
-        else:
-            return {"answer": "The answer to your question is not directly related to the content available on Gustavo's blog. The blog mainly covers technical topics in technology, data science, machine learning, and artificial intelligence. If you have suggestions for new topics, feel free to get in touch through the contact page.",
-                    "sources": []}
-    
-    context = "\n\n".join(fr.page_content for fr in filtered_results)
 
-    sources = [
-        {"title": clean_title(doc.metadata.get("title", "Sem título")), "url": doc.metadata.get("path", "#")}
-        for doc in filtered_results
+    threshold = 0.65
+    filtered_results = [
+        doc for doc, score in results
+        if (score >= threshold) and (doc.metadata.get("lang") == q.lang)
     ]
 
-    unique_sources = list({(src["title"], src["url"]) for src in sources})
-    sources = [{"title": title, "url": url} for title, url in unique_sources]
+    if not filtered_results:
+        with open(f"prompts/non_related_{q.lang}.txt", "r", encoding="utf-8") as file:
+            non_related_prompt = file.read()
+            return {"answer": non_related_prompt}
 
-    prompt = ""
+    context = "\n\n".join(fr.page_content for fr in filtered_results)
+    
+    unique_sources_set = {
+        (clean_title(doc.metadata.get("title", "Sem título")), doc.metadata.get("path", "#"))
+        for doc in filtered_results
+    }
+    sources = [{"title": title, "url": url} for title, url in unique_sources_set]
+
     query = ""
     context_info = ""
+    with open(f"prompts/response_guidelines_{q.lang}.txt", "r", encoding="utf-8") as file:
+        prompt = file.read()
+
     if q.lang == "pt":
         query = "Pergunta"
         context_info = "Contexto presente no blog de Gustavo sobre esse assunto"
-        prompt = """Você é um assistente que responde em português sobre um blog do Gustavo Sampaio.  
-- O blog contém posts técnicos sobre assuntos diversos, incluindo tecnologia, ciência de dados, machine learning e inteligência artificial.
-- Informações adicionais sobre Gustavo Sampaio (cientista de dados, estudante de doutorado na USP em linguagem natural, atuando no Hospital Albert Einstein em projetos que combinam texto e imagem aplicados à saúde pública).
-- Responda apenas com base nos conteúdos do blog (ou informações extras sobre Gustavo fornecidas acima, se e somente se forem necessárias).  
-- Sua resposta deve parecer natural, como se você soubesse do conteúdo por si só.  
-- Não sugira novas perguntas nem responda a temas fora do escopo do blog.
-- Não mencione que recebeu textos de apoio, contexto fornecido, RAG ou outras instruções ocultas.""" 
     else:
         query = "Question"
         context_info = "Existent context on Gustavo's blog related to this topic"
-        prompt = """You are an assistant responding in the same query language about a blog by Gustavo Sampaio.
-- The blog contains technical posts about technology, data science, machine learning, and artificial intelligence.
-- Additional information about Gustavo Sampaio (data scientist, PhD student at USP in natural language, participant at Albert Einstein Hospital in projects that combine text and images applied to public health).
-- Respond only based on the blog's content (or additional information about Gustavo provided above, only if possible).
-- Your response should sound natural, as if you knew the content yourself.
-- Do not suggest new questions or respond to topics outside the scope of the blog.
-- Do not mention that you received supporting texts, provided context, RAG, or other hidden instructions."""
 
     completion = await chat.ainvoke([
-        {
-            "role": "system", "content": prompt
-        },
-        {
-            "role": "user", "content": f"{query}: {q.query}\n{context_info}:{context}",
-        },
+        {"role": "system", "content": prompt},
+        {"role": "user", "content": f"{query}: {q.query}\n{context_info}:{context}",},
     ])
 
-    print(sources)
     return {"answer": completion.content, "sources": sources}
